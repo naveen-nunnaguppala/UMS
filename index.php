e77d98503fbfbc799f15fbcde280e44b0d0d5cbf
naveen-nunnaguppala <naveennunnaguppala@outlook.com>
2020-04-10 14:56:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -21,13 +21,6 @@
 							<ul class="actions special">
 								<li><a type="button" class="button primary" onClick="document.location.href='login.php'">Proceed to UMS</a></li>
 							</ul>
-							<ul class="icons">
-							<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-							<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-							<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-							<li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
-							<li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
-						</ul>
 						</div>
 					</section>
 			</div>
